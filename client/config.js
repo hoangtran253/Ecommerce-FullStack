2cf9994bdfbba8335e960367fe8d0320bc9dc493
duchoang <duchoangpro34@gmail.com>
2025-11-24 12:36:43 +0700

    Initial Commit

--- client/config.js
+++ client/config.js
@@ -1,23 +1,24 @@
-export const serverUrl = import.meta.env.VITE_BACKEND_URL;
+const VERCEL_BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+export const serverUrl = VERCEL_BACKEND_URL || "http://localhost:8000";
 
 const checkConfig = (server) => {
-  let config = {};
-  switch (server) {
-    case "production":
-      config = {
-        baseUrl: "http://localhost:8000",
-      };
-      break;
-    case "local":
-      config = {
-        baseUrl: "http://localhost:8000",
-      };
-      break;
-    default:
-      break;
-  }
-  return config;
+  let config = {};
+  switch (server) {
+    case "production": 
+      config = {
+        baseUrl: VERCEL_BACKEND_URL, // SỬ DỤNG BIẾN MÔI TRƯỜNG
+      };
+      break;
+    case "local":
+      config = {
+        baseUrl: "http://localhost:8000",
+      };
+      break;
+    default:
+      break;
+  }
+  return config;
 };
 
-export const selectServer = "local";
+export const selectServer = "production"; 
 export const config = checkConfig(selectServer);
